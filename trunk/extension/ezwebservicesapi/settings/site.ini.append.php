e000db31d63a78b5ce60ca7f8738a8c0446c4fe9
Gaetano Giunta <gg@ez.no>
2010-05-11 16:08:58 +0000

    - allow extension's cache to be cleared
    git-svn-id: http://svn.projects.ez.no/ezwebservicesapi@4 5cf9c3b6-ae6f-4fab-b805-61d6bc67f7f7

---
+++ trunk/extension/ezwebservicesapi/settings/site.ini.append.php
@@ -0,0 +1,11 @@
+<?php /*
+
+# Cache item entry (for eZ Publish 4.3 and up)
+[Cache]
+CacheItems[]=ezwebservicesapi
+
+[Cache_ezwebservicesapi]
+name=eZWebservicesAPI extension cache (fetch/view webservices list)
+path=ezwebservicesapi
+
+*/ ?>
