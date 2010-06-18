d4ad63b6d8f71534bad0e978f5ebb02bb5a83a90
Gaetano Giunta <gg@ez.no>
2010-06-18 12:54:37 +0000

    - update docs; - tag for 0.1 release
    git-svn-id: http://svn.projects.ez.no/ezwebservicesapi@9 5cf9c3b6-ae6f-4fab-b805-61d6bc67f7f7

--- trunk/extension/ezwebservicesapi/ezinfo.php
+++ trunk/extension/ezwebservicesapi/ezinfo.php
@@ -6,7 +6,7 @@ class ezwebservicesapiInfo
     {
         return array(
             'Name' => "eZ Webservices API extension",
-            'Version' => "0.1-dev",
+            'Version' => "0.1",
             'Copyright' => "Copyright (C) 2010 Gaetano Giunta",
             'License' => "GNU General Public License v2.0"
         );
