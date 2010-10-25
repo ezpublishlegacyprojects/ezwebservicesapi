45ef3f3aef8322d9f800c5c62027a2d038818ac0
Gaetano Giunta <gg@ez.no>
2010-10-25 00:21:22 +0000

    - update info file
    git-svn-id: http://svn.projects.ez.no/ezwebservicesapi@19 5cf9c3b6-ae6f-4fab-b805-61d6bc67f7f7

--- trunk/extension/ezwebservicesapi/ezinfo.php
+++ trunk/extension/ezwebservicesapi/ezinfo.php
@@ -5,7 +5,7 @@ class ezwebservicesapiInfo
     static function info()
     {
         return array(
-            'Name' => "eZ Webservices API extension",
+            'Name' => "<a href=\"http://projects.ez.no/ezwebservicesapi\">eZ Webservices API extension</a>",
             'Version' => "0.2-dev",
             'Copyright' => "Copyright (C) 2010 Gaetano Giunta",
             'License' => "GNU General Public License v2.0"
