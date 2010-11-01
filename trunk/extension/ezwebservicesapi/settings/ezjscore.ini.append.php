0bf6bea881b5c432de5a46b817330ce6c9f8d02f
Gaetano Giunta <gg@ez.no>
2010-11-01 23:15:59 +0000

    - more docs
    git-svn-id: http://svn.projects.ez.no/ezwebservicesapi@24 5cf9c3b6-ae6f-4fab-b805-61d6bc67f7f7

--- trunk/extension/ezwebservicesapi/settings/ezjscore.ini.append.php
+++ trunk/extension/ezwebservicesapi/settings/ezjscore.ini.append.php
@@ -7,11 +7,11 @@
 # Example urls to test this server functions from a browser:
 # <root>/ezjscore/call/ezp::inspect::<...to_be_determined...>
 # <root>/ezjscore/call/ezp::fetch_content_list::{"parent_node_id":2}?ContentType=json
-# <root>/ezjscore/call/ezp::operation_content_hide::[2]?ContentType=json
+# <root>/ezjscore/call/ezp::operation_content_hide::{"node_id":2}?ContentType=json
 
 [ezjscServer_ezp]
-# actual class is ezWebservicesAPIJSCFunctions, but it is built dynamically
-# to allow us to introspect existing
+# actual class is a subclass of ezWebservicesAPIJSCFunctions, it is built
+# dynamically to allow us to inject into it new methods exposed as services
 Class=ezWebservicesAPIJSCFunctionsExtended
 File=extension/ezwebservicesapi/classes/ezwebservicesapijscfunctionsextender.php
 
